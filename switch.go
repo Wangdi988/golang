48ae285ab69a0bf3a051a2a1cad0a98643a7a39c
Sangay Wangdi <sangayw@dk.b>
2026-01-22 23:01:26 +0600

    changes

---
+++ switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	day := 6
+
+	switch day {
+	case 1:
+		fmt.Println("first day")
+	case 2: 
+		fmt.Println("Second day")
+	
+	default:
+		fmt.Println("None")
+	}
+}
